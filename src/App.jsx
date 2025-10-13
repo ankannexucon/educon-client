2a3daf81d6949daf92503585a3d15117e3aaed01
Shubhodeep Polley <subhodeep.polley@nexucon.com>
2025-10-13 19:48:25 +0530

    this is chatbot global

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import DocumentVerification from './components/DocumentVerification';
-import EduconChatbot from './components/EduconChatbot';
-import Index from './routes/Index';
+import React from "react";
+import DocumentVerification from "./components/DocumentVerification";
+import EduconChatbot from "./components/EduconChatbot";
+import Index from "./routes/Index";
 
 function App() {
   return (
     <div>
-      <Index/>
+      <Index />
+      <EduconChatbot />
     </div>
   );
 }
