c3976a81e14718c1861d30618b8b67b6e5de36f1
Shubhodeep Polley <subhodeep.polley@nexucon.com>
2025-10-14 14:20:06 +0530

    Add Admission and CVBuilder pages to routing

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavbarComponent from "../components/navigation/NavbarComponent";
 import DocumentVerification from "../components/DocumentVerification";
 import FooterComponent from "../components/footer/FooterComponent";
+import AdmissionPage from "../pages/Admission/admissionPage";
+import CVBuilder from "../pages/profile/CVBuilder";
 
 // Lazy imports
 const LandingPage = lazy(() => import("../pages/dashboard/LandingPage"));
@@ -23,8 +25,9 @@ export default function Index() {
         <Route path="/signup" element={<AuthPage mode={"signup"} />} />
         <Route path="/login" element={<AuthPage mode={"login"} />} />
         <Route path="/profile" element={<UserProfile />} />
+
         <Route path="/fraude-detection" element={<DocumentVerification />} />
-        
+        <Route path="/admissions" element={<AdmissionPage />} />
         <Route path="/university" element={<UniversityDashboardPage />} />
         <Route path="/courses" element={<CoursesPage />} />
       </Routes>
